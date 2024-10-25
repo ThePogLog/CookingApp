7051d3a107a62ccd8933594eba85ce74b1e54d31
klachag <26.kael.lachaga@ksd.org>
2024-10-25 09:57:40 -0700

    ITS DONE
    the stupid claculator is done just make it look nice please

--- ChefAppWithARat/Assets/CompleteTotal.cs
+++ ChefAppWithARat/Assets/CompleteTotal.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CompleteTotal : MonoBehaviour
 {
@@ -19,13 +20,35 @@ public class CompleteTotal : MonoBehaviour
     public Text total6;
     [SerializeField]
     public Text FINALTOTAL;
-    [SerializeField]
+    
     float TOTALPRICE;
+
+    float tot1; 
+
+    float tot2;
+    float tot3;
+    float tot4;
+    float tot5;
+    float tot6;
+
     // Start is called before the first frame update
     void Start()
     {
-        TOTALPRICE = total1.ToString(); 
-        FINALTOTAL.text = TOTALPRICE;
+    }
+    public void theCost()
+    {
+        float.TryParse(FINALTOTAL.text, out TOTALPRICE);
+        float.TryParse(total1.text, out tot1);
+        float.TryParse(total2.text, out tot2);
+        float.TryParse(total3.text, out tot3);
+        float.TryParse(total4.text, out tot4);
+        float.TryParse(total5.text, out tot5);
+        float.TryParse(total6.text, out tot6);
+
+
+        TOTALPRICE = tot1 + tot2 + tot3 + tot4 + tot5 + tot6;
+        FINALTOTAL.text = TOTALPRICE.ToString();
+
     }
 
     // Update is called once per frame
